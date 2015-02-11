1b7273eb4907e045a5d789d5670c32d48c491aba
Kazushige TAKEUCHI <kazushige.takeuchi+gh@gmail.com>
2015-02-12 01:45:43 +0900

    for my template

--- paster_ansible/template.py
+++ paster_ansible/template.py
@@ -4,4 +4,5 @@ from paste.script import templates
 class AnsibleProjectTemplate(templates.Template):
     summary = 'Ansible Best Practice Template: http://docs.ansible.com/playbooks_best_practices.html#directory-layout'
     _template_dir = 'templates/default_project'
+    use_cheetah = True
     pass
